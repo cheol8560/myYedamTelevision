7cd0c03f883dcb1a78f3d34bd304256197876e51
yongjin <kyj6578@naver.com>
2017-06-14 17:51:47 +0900

    6/14 17:50 용진 프로시져 추가, insertTranslatePoint.jsp translatePointResult.jsp, TransVO 추가 translateController 에 insert, update, delete 추가

--- myYedamTelevision/src/main/java/yolo/myTv/translatePoints/service/impl/TranslatePointServiceImpl.java
+++ myYedamTelevision/src/main/java/yolo/myTv/translatePoints/service/impl/TranslatePointServiceImpl.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import yolo.myTv.translatePoints.service.TransVO;
 import yolo.myTv.translatePoints.service.TranslatePointService;
 import yolo.myTv.translatePoints.service.TranslatePointVO;
 
@@ -19,6 +20,30 @@ public class TranslatePointServiceImpl implements TranslatePointService {
 	@Override
 	public List<Map<String, Object>> totalPoint(TranslatePointVO vo) {	
 		return TranslatePointDAO.totalPoint(vo);
+	}
+
+	@Override
+	public List<TranslatePointVO> getTranslatePointList(TranslatePointVO vo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateTranslatePoint(TranslatePointVO vo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteTranslatePoint(TranslatePointVO vo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	//거래발생 프로시져
+	@Override
+	public void insertTrans(TransVO vo) {
+		TranslatePointDAO.insertTrans(vo);
 	}
  
 }
